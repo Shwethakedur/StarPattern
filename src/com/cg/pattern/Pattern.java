d844b74a02a945935b9a5c0710b8c1e1f1cbc39c
Shwethakedur <38555293+Shwethakedur@users.noreply.github.com>
2018-04-23 12:47:31 +0530

    StarPattern

---
+++ src/com/cg/pattern/Pattern.java
@@ -0,0 +1,37 @@
+package com.cg.pattern;
+
+public class Pattern {
+	
+	
+	public static void main(String args[])
+	{
+
+		for (int i=1; i<=3; i++)
+		{
+		for (int j=1; j<=3; j++)
+		{
+			
+		if(i==1 && j==1 || i==1 && j==3 || i==2 && j==3)
+		{
+			System.out.print("  ");
+			
+		}
+		else
+		{
+			
+				
+			System.out.print("* ");
+		}
+		
+	}
+		System.out.print("\n");
+	}
+}
+}
+		
+
+		
+		
+	
+
+
